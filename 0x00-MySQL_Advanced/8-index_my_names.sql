119138de2d762aa88f9d12ef0755d775efb4a96a
AdwoaSK <106457557+AdwoaSK@users.noreply.github.com>
2024-12-23 15:25:33 +0000

    Revert "Add task 8"
    This reverts commit b906d07ed132be636a67bf3b5c11f02ee26e95a4.

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++
@@ -1,4 +0,0 @@
--- Task: Create an index on the first letter of the `name` column in the `names` table.
-
--- Create an index named `idx_name_first` on the first letter of the `name` column
-CREATE INDEX idx_name_first ON names (name(1));
